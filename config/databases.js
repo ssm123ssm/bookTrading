1fcc5e5eb43c3b37bfa95d1f4639d98b5c3cc346
Supun Sudaraka Manathunga <ssm123ssm@gmail.com>
2017-08-07 21:34:53 +0530

    Update databases.js

--- config/databases.js
+++ config/databases.js
@@ -1,4 +1,4 @@
 module.exports = {
-    usersURL: 'mongodb://ssm123ssm:chandrani123@ds163397.mlab.com:63397/books',
-    dbURL: 'mongodb://localhost:27017/data'
+    usersURL: MONGO,
+    dbURL: DATA
 }
