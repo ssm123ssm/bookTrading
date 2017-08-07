3861fbc192cf46c83ec3c38bbb9ae4ccb8f0a0c8
Supun Sudaraka Manathunga <ssm123ssm@gmail.com>
2017-08-07 21:35:39 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,3 @@
 module.exports = {
-    clientID: '250302982144128',
-    clientSecret: '73a0e8e6cc947874b3341f73404a6cb2',
-    callbackURL: '/auth/callback',
-    profileFields: ['id', 'displayName', 'photos', 'emails']
+    
 }
